8085ff9039dc829bcffbf1fcd8e5ff118fea797a
Alberto Revel <albertorevel@users.noreply.github.com>
2018-10-21 23:57:14 +0200

    Moving with axis

--- PR1S/UIViewDraw.swift
+++ PR1S/UIViewDraw.swift
@@ -54,15 +54,60 @@ class UIViewDraw: UIView {
         line.addLine(to: CGPoint(x:  x0, y: y1))
         context.addPath(line)
         
+       /* Y AXIS */
+        
+        
+        // Calculate sizes
+        let y_axis_segments:CGFloat = CGFloat((m_y_axis_max_value - m_y_axis_min_value ) / m_y_axis_step)
+        let y_axis_segment:CGFloat = (y0 - y1) / y_axis_segments
+        
+        
+        // We define text attributes
+        let stringStyle = NSMutableParagraphStyle()
+        stringStyle.alignment = .right
+    
+        let attributes = [
+            NSAttributedString.Key.paragraphStyle: stringStyle
+        ]
+        
+        var attributedString:NSAttributedString
+        var textbox:CGRect
+        var stringHeight:CGFloat
+        var textY:CGFloat = y0
+        var drawingY:CGFloat
+        
+        for i in stride(from: (m_y_axis_min_value + m_y_axis_step), through: m_y_axis_max_value,
+                        by: m_y_axis_step) {
+            
+            
+            attributedString = NSAttributedString(string : String(format: "%.0f", i), attributes: attributes)
+            stringHeight = attributedString.size().height
+            
+            textY -= CGFloat(y_axis_segment)
+            drawingY = textY - stringHeight / 2
+            
+            textbox = CGRect(x: 0, y: drawingY, width: x0 - 20, height: stringHeight)
+            attributedString.draw(in: textbox)
+                            
+            line.move(to: CGPoint(x: x0 - 5, y : textY))
+            line.addLine(to: CGPoint(x:  x0 + 5, y: textY))
+            context.addPath(line)
+                            
+        }
+        
         context.strokePath()
         
-        // We draw texts
+        /* X_AXIS */
         
-        let string = "100"
-        string.draw(in: CGRect(x: 0,y: y0, width: 100, height: 100))
+        // Calculate sizes
+        let x_axis_segment:CGFloat = (x1 - x0) / CGFloat(m_x_axis_labels?.count ?? 1)
         
+        var stringWidth:CGFloat
+        var drawingX:CGFloat
         
-        
+        for label in m_x_axis_labels ?? NSMutableArray() {
+            
+        }
         // END-CODE-UOC-3
     }
 
